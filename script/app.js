a9304b9313c4814bc1c96a2d8d98a5fa905b9989
poria neyshaboriyan <poria.nsh@gmail.com>
2024-06-12 23:24:03 +0330

    compelet project

--- script/app.js
+++ script/app.js
@@ -9,6 +9,7 @@ submenumobail.addEventListener("click", function (e) {
   document.querySelector("#arrow--active").classList.toggle("rotate-180");
 });
 overlay.addEventListener("click", function (e) {
+  document.body.classList.remove('overflow-y-hidden')
   e.currentTarget.classList.remove("overlay--open");
   if (cartmobail.classList.contains("cart-mobail--open")) {
     cartmobail.classList.remove("cart-mobail--open");
@@ -23,9 +24,11 @@ function Conditionnav() {
   if (navmobail.classList.contains("nav-mobail--open")) {
     navmobail.classList.remove("nav-mobail--open");
     overlay.classList.remove("overlay--open");
+    document.body.classList.remove('overflow-y-hidden')
   } else {
     navmobail.classList.add("nav-mobail--open");
     overlay.classList.add("overlay--open");
+    document.body.classList.add('overflow-y-hidden')
   }
 }
 
@@ -35,9 +38,12 @@ function Conditioncart() {
   if (cartmobail.classList.contains("cart-mobail--open")) {
     cartmobail.classList.remove("cart-mobail--open");
     overlay.classList.remove("overlay--open");
+    document.body.classList.remove('overflow-y-hidden')
+
   } else {
     cartmobail.classList.add("cart-mobail--open");
     overlay.classList.add("overlay--open");
+    document.body.classList.add('overflow-y-hidden')
   }
 }
 
